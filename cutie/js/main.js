9cd64fc3ac477d1365c27ee05381bf79f424df94
Keasalte <jaychen1347@gmail.com>
2023-12-08 13:18:33 -0500

    change ideas

--- cutie/js/main.js
+++ cutie/js/main.js
@@ -1,5 +1,5 @@
-
-const URL = "http://shibe.online/api/shibes?count=100&urls=true&httpsUrls=true"
+//B7IMQ6V9OVTI4OFE
+const URL = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=B7IMQ6V9OVTI4OFE"
 
 const nom = [];
 
@@ -15,14 +15,22 @@ async function getData(URL){
     const data = await response.json();
     console.log(data);
     nom.push(data);
-    console.log
-
+    console.log(nom);
+    console.log(Object.keys(nom[0]["Monthly Time Series"]));
   }
   catch (error) {
     document.querySelector("h1").textContent = error;
   }
 }
 getData(URL);
+
+/* let rando = Math.random() *  */
+dates = Object.keys(nom[0]["Monthly Time Series"])
+const nom1 = [];
+nom1.push(dates);
+console.log(dates);
+x = Math.floor(Math.random() * dates.length);
+console.log(x);
 
 /*     let contain = document.querySelector('#app');
     data.forEach((picturelink)=> contain.insertAdjacentHTML(
