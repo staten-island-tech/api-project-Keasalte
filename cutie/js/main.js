531de4cd28dedf7664235d32274e6748cda3c858
Keasalte <jaychen1347@gmail.com>
2024-01-06 18:58:02 -0500

    cool

--- cutie/js/main.js
+++ cutie/js/main.js
@@ -1,17 +1,17 @@
-//B7IMQ6V9OVTI4OFE
-//5E6XQ7EEC3KSRLTP
-//lookup selectelement
-//https://www.w3schools.com/js/js_graphics_chartjs.asp
 
-//const URL = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=NVDA&apikey=5E6XQ7EEC3KSRLTP"
-const URL ="https://api.stockdata.org/v1/data/eod?symbols=TSLA&api_token=b1WoSzHY2m7tsLycpVyt3CywMOGNIzgSD8JB4UB6";
-const nom = [];
+//https://www.w3schools.com/js/js_graphics_chartjs.asp
+//2daKFNLswJB2H6mxCEMAi08YV63ws4AdKjsoAhHL
+//b1WoSzHY2m7tsLycpVyt3CywMOGNIzgSD8JB4UB6
+//OYuAjtm7D4Wm4zYl3fNKZR8gJtZF3w5MT0qzeEye
+let URL ="https://api.stockdata.org/v1/data/eod?symbols=TSLA&api_token=OYuAjtm7D4Wm4zYl3fNKZR8gJtZF3w5MT0qzeEye";
+let nom = [];
 let nom1 = [];
 let nom2 = [];
 let nom3 = [];
 let count = 0;
 let count2 = 0;
-
+let k = 0;
+let s = 0;
 let charting = null;
 
 const DOM = {
@@ -19,13 +19,13 @@ const DOM = {
   Down: document.getElementById("colorthing2"),
   count: document.getElementById("counter"),
   count2: document.getElementById("counter2"),
+  count3: document.getElementById("counter3"),
   chart: document.getElementById("Chart"),
+  'PZZA': document.getElementById('PZZA'),
+  'AMZN': document.getElementById('AMZN'),
+  'NVDA': document.getElementById('NVDA'),
+  'TSLA': document.getElementById('TSLA'),
 };
-
-function Update(){
-  DOM.count.innerText = `Win Counter: ${count}`;
-  DOM.count2.innerText = `Lose Counter: ${count2}`;
-}
 
 async function getData(URL) {
   try {
@@ -39,7 +39,13 @@ async function getData(URL) {
     console.log(nom);
     console.log(Object.keys(nom[0]["data"]));
   } catch (error) {
-    document.querySelector("h1").textContent = error;
+    document.querySelector('body').insertAdjacentHTML(
+      "beforeend",
+      `<dialog class="card">
+        <h2 class = "text">${error}, Please Retry.</h2>
+        <button id="close" autofocus> click to return </button>
+      </dialog>`
+      );
   }
 }
 
@@ -56,10 +62,10 @@ async function getRandomDates(nom) {
       nom1.push(dates[i]);
       console.log(nom1);
     }
-    console.log(nom1);
+    console.log(nom1); //random numbers to grab data
 
     nom1.forEach((closing) => {
-      nom2.push(nom[0]["data"][closing]["close"]);
+      nom2.push(nom[0]["data"][closing]["close"]); //get the closing value for the day
     });
     console.log(nom2);
     console.log("hehehehaw");
@@ -67,9 +73,15 @@ async function getRandomDates(nom) {
     nom1.forEach((date) =>
       nom3.push(nom[0]["data"][date]["date"].slice(0, 10))
     );
-    console.log(nom3);
+    console.log(nom3); //random dates; doesn't matter
   } catch (error) {
-    document.querySelector("h1").textContent = error;
+    document.querySelector('body').insertAdjacentHTML(
+      "beforeend",
+      `<dialog class="card">
+        <h2 class = "text">${error}, Please Retry.</h2>
+        <button alt = "click escape to return" id="close" class="button-1" autofocus> click to return </button>
+      </dialog>`
+      );
   }
 }
 
@@ -91,11 +103,32 @@ async function chart() {
         ],
       },
       options: {
-        legend: { display:true, position:'top',},
-      },
+        responsive: true,
+        maintainAspectRatio:false,
+        plugins: {
+        title:{
+          display:true,
+          text: 'Graph of stock price for 5 consecutive days',
+          padding: {
+            top:10,
+            boottom:30,
+          }
+        },
+        legend: { display:false
+        },
+        tooltips:{
+          enabled:false
+        }
+      },}
     });
   } catch (error) {
-    document.querySelector("h1").textContent = error;
+    document.querySelector('body').insertAdjacentHTML(
+      "beforeend",
+      `<dialog class="card">
+        <h2 class = "text">${error}, Please Retry.</h2>
+        <button alt = "click escape to return" id="close" class="button-1" autofocus> click to return </button>
+      </dialog>`
+      );
   }
 }
 
@@ -104,51 +137,58 @@ async function nextValue(){
     await chart();
     const seep = nom1[4];
     const keep =  parseInt(seep) + 1
-    const s = nom[0]["data"][seep]["close"]
-    const k = nom[0]["data"][keep]["close"] 
+    s = nom[0]["data"][seep]["close"]
+    k = nom[0]["data"][keep]["close"] 
     console.log(s);
     console.log(k);
-    DOM.Up.addEventListener("click",function (){
-      if( k > s ){
-        Win();
-        resetchart();
-      }
-      else{
-        Lose();
-        resetchart();
-      }
-    })
-    DOM.Down.addEventListener("click",function (){
-      if( k < s ){
-        Win();
-        resetchart();
-      }
-      else{
-        Lose();
-        resetchart();
-      }
-    })
-
   } catch (error) {
-    document.querySelector("h1").textContent = error;
+    document.querySelector('body').insertAdjacentHTML(
+      "beforeend",
+      `<dialog class="card">
+        <h2 class = "text">${error}, Please Retry.</h2>
+        <button alt = "click escape to return" id="close" class="button-1" autofocus> click to return </button>
+      </dialog>`
+      );
   }
 }
 
+DOM.Up.addEventListener("click",function (){
+  if( k > s ){
+    Win();
+    resetchart();
+  }
+  else{
+    Lose();
+    resetchart();
+  }
+})
+DOM.Down.addEventListener("click",function (){
+  if( k < s ){
+    Win();
+    resetchart();
+  }
+  else{
+    Lose();
+    resetchart();
+  }
+})
+
 nextValue();
 
-
 function resetchart(){
-  charting.destroy();
-chart();
-DOM.chart.update();
+charting.destroy();
+nom = [];
+nextValue();
 } 
 
 function Win(){
   document.querySelector('body').insertAdjacentHTML(
     "beforeend",
     `<dialog class="card">
-      <h2 class = "text">you winn</h2>
-      <button id="close" autofocus> click to return </button>
+      <h2 class = "text">You were right!</h2>
+      <h3 class = "text"> Previous Day's Closing Price: ${s}</h3>
+      <h3 class = "text"> The Next Day's Closing Price: ${k}</h3>
+      <button alt = "click escape to return" id="close" class="button-1" autofocus> click to return </button>
     </dialog>`
   );
   document.querySelector("dialog").showModal();
@@ -165,8 +205,10 @@ function Lose(){
   document.querySelector('body').insertAdjacentHTML(
     "beforeend",
     `<dialog class="card">
-      <h2 class = "text">you losee</h2>
-      <button id="close" autofocus> click to return </button>
+      <h2 class = "text">Unfortunately you lost the 50/50.</h2>
+      <h3 class = "text"> Previous Day's Closing Price: ${s}</h3>
+      <h3 class = "text"> The Next Day's Closing Price: ${k}</h3>
+      <button alt = "click escape to return" id="close" class="button-1" autofocus> click to return </button>
     </dialog>`
   );
   document.querySelector("dialog").showModal();
@@ -178,3 +220,18 @@ function Lose(){
   })
 }
 
+function Update(){
+  DOM.count.innerText = `Win Counter: ${count}`;
+  DOM.count2.innerText = `Lose Counter: ${count2}`;
+}
+
+function changeURL(comp) {DOM[comp].addEventListener("click", function(){
+URL = `https://api.stockdata.org/v1/data/eod?symbols=${comp}&api_token=OYuAjtm7D4Wm4zYl3fNKZR8gJtZF3w5MT0qzeEye`
+console.log(URL);
+DOM.count3.innerText = `Current Company ${comp}`
+})}
+
+changeURL('PZZA');
+changeURL('AMZN');
+changeURL('NVDA');
+changeURL('TSLA');
